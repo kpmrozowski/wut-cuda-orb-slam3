f8cf0658dbd55ec04f66c332c5b1ad9381a36299
Bednarek Mikołaj <mikolaj@bednarek.xyz>
2022-05-23 23:34:50 +0200

    some cl attempts

---
+++ include/OpenCL/Orb.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef __ORB_HPP__
+#define __ORB_HPP__
+
+#include <vector>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/opencl/ocl_defs.hpp>
+#include <clc/clc.h>
+#include <boost/compute.hpp>
+
+using GpuKeyPoints = boost::compute::vector<cv::KeyPoint>;
+
+constexpr auto g_openCLProgram = BOOST_COMPUTE_STRINGIZE_SOURCE(
+        __kernel void squareVector(__global int *values) {
+                const int globalId = get_global_id(0); // groupId * size  + local
+                const int groupId = get_group_id(0); // upto 8
+                const int localId = get_local_id(0); // upto 256
+                const int value = values[globalId];
+                values[globalId] = value * groupId;
+        }
+);
+
+namespace ORB_SLAM3::opencl {
+
+class GpuOrb {
+    unsigned int m_maxKeyPoints;
+    GpuKeyPoints m_gpuKeyPoints;
+    cv::UMat m_gpuDescriptors;
+    cv::UMat m_gpuDesc;
+    cv::ocl::Queue m_queue;
+  public:
+    GpuOrb(int maxKeyPoints = 10000);
+    ~GpuOrb();
+
+    void launch_async(cv::InputArray _image, const cv::KeyPoint * _keyPoints, const int nPoints);
+    void join(cv::Mat &_descriptors);
+
+    static void loadPattern(const cv::Point * _pattern);
+};
+
+}
+#endif
---
+++ src/OpenCL/Fast.cpp
@@ -0,0 +1,28 @@
+#include <boost/compute.hpp>
+#include <OpenCL/Fast.hpp>
+#include <OpenCL/Manager.hpp>
+
+namespace ORB_SLAM3::opencl {
+
+void IC_Angle::launch_async(const cv::_InputArray &_image, cv::KeyPoint *_keyPoints, int nPoints, int half_k,
+                            int minBorderX, int minBorderY, int octave, int size)
+{
+    namespace compute = boost::compute;
+    auto imageMatrix  = _image.getUMat();
+
+    auto &gpuMan = Manager::the();
+
+    compute::vector<cv::KeyPoint> gpuKeyPoints(nPoints);
+    compute::copy(_keyPoints, _keyPoints + nPoints, gpuKeyPoints.begin());
+
+    auto addBorderRes = gpuMan.run(Program::AddBorder, "addBorder_kernel", nPoints, minBorderX, minBorderY, octave, size);
+
+    auto angleRes = gpuMan.run(Program::AngleKernel, "IC_Angle_kernel", nPoints, minBorderX, minBorderY, octave, size);
+
+    addBorderRes.wait();
+    angleRes.wait();
+
+    compute::copy(_keyPoints, _keyPoints + nPoints, gpuKeyPoints.begin());
+}
+
+}// namespace ORB_SLAM3::opencl
